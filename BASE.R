0489c810f5c9841974b112894ba7f2af4bacb430
gyoo1 <gihyun@ualberta.ca>
2022-03-15 14:49:36 -0400

    everything combined into a single function

--- BASE.R
+++ BASE.R
@@ -1,19 +1,18 @@
 ## Determine number of bases in a DNA sequence ##
 
-# 1. Read DNA sequence
-read.DNA <- function(FileName){
+bp.DNA <- function(FileName){
+        #Read DNA sequence
         DNAfile <- paste("./", FileName, ".seq", sep = "")
         DNAseq <- scan(file = DNAfile, what = "list")
         print(DNAseq)
-}
-
-# 2. Determine number of bases
-bp.DNA <- function(DNAseq, FileName){
+        
+        #Count number of bases
         Aa = lengths(regmatches(DNAseq[2], gregexpr("[Aa]", DNAseq[2])))
         Gg = lengths(regmatches(DNAseq[2], gregexpr("[Gg]", DNAseq[2])))
         Cc = lengths(regmatches(DNAseq[2], gregexpr("[Cc]", DNAseq[2])))
         Tt = lengths(regmatches(DNAseq[2], gregexpr("[Tt]", DNAseq[2])))
         
+        #Write results
         output <- paste(Aa, Gg, Cc, Tt, sep = ", ") #number of A, G, C, T bases in sequence
         countfile = paste0(FileName, ".count")
         write(output, file = countfile) #output file
@@ -21,5 +20,4 @@ bp.DNA <- function(DNAseq, FileName){
 }
 
 #Example
-# DNAseq <- read.DNA("Random_Sequence")
-# bp.DNA(DNAseq, "Random_Sequence")
+# bp.DNA("Random_Sequence")
